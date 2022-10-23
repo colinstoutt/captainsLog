2c8fee97d041591eff159ac967c2e2a78df8737e
cps97 <colinstoutt@gmail.com>
2022-10-22 17:38:01 -0700

    edit mongoose.connect

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-const CaptainsLog = require("./models/logs");
 const logController = require("./controllers/logs");
 
 require("dotenv").config();
 const PORT = process.env.PORT;
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -16,7 +18,7 @@ app.use("/logs", logController);
 
 const db = mongoose.connection;
 db.on("error", (err) => console.log(err.message + " is mongo not running?"));
-db.on("connected", () => console.log("mongo connected"));
-db.on("disconnected", () => console.log("mongo disconnected"));
+db.on("connected", () => console.log("Mongo Connected"));
+db.on("disconnected", () => console.log("Mongo Disconnected"));
 
 app.listen(PORT, () => console.log(`Captains Log Active(${PORT})`));
