dfb48950d69b3f4a2044f764aad5cfe22056a196
cps97 <colinstoutt@gmail.com>
2022-10-22 17:36:18 -0700

    add functioning create route, new route

--- controllers/logs.js
+++ controllers/logs.js
@@ -2,27 +2,33 @@ const express = require("express");
 const logRouter = express.Router();
 const CaptainsLog = require("../models/logs");
 
-//// ===INDUCES===
 // ===index===
 logRouter.get("/", (req, res) => {
-  res.render("index.ejs");
+  CaptainsLog.find({}, (err, allLogs) => {
+    res.render("index.ejs", {
+      logs: allLogs,
+    });
+  });
 });
+
 // ===new===
 logRouter.get("/new", (req, res) => {
   res.render("new.ejs");
 });
+
 // ===create===
 logRouter.post("/", (req, res) => {
-  res.send(req.body);
   if (req.body.shipIsBroken === "on") {
-    //if checked, req.body.completed is set to 'on'
     req.body.shipIsBroken = true;
   } else {
     req.body.shipIsBroken = false;
   }
-  //   CaptainsLog.create(req.body, (error, createdLog) => {
-  //     res.redirect("/captains-log");
-  //   });
+  CaptainsLog.create(req.body, (err, createdLog) => {
+    console.log(createdLog);
+    res.redirect("/logs");
+  });
 });
+
+// ===show===
 
 module.exports = logRouter;
